43518957ccc2a2f80a7cabab148e3b36fa0d6677
Jack Hance <derp332211@gmail.com>
2017-01-21 20:12:05 -0600

    Add project files.

---
+++ KinectLiveUpdate/MainForm.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace KinectLiveUpdate {
+    partial class MainForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.label_info = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.SystemColors.ControlText;
+            this.pictureBox.Location = new System.Drawing.Point(12, 34);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(640, 480);
+            this.pictureBox.TabIndex = 5;
+            this.pictureBox.TabStop = false;
+            // 
+            // label_info
+            // 
+            this.label_info.AutoSize = true;
+            this.label_info.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_info.Location = new System.Drawing.Point(12, 9);
+            this.label_info.Name = "label_info";
+            this.label_info.Size = new System.Drawing.Size(189, 19);
+            this.label_info.TabIndex = 6;
+            this.label_info.Text = "Kinect Live Body Model";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 526);
+            this.Controls.Add(this.label_info);
+            this.Controls.Add(this.pictureBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Kinect BodyModel";
+            this.Load += new System.EventHandler(this.MainForm_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label label_info;
+    }
+}
+
---
+++ KinectLiveUpdate/MainForm.cs
@@ -0,0 +1,152 @@
+﻿
+
+namespace KinectLiveUpdate
+{
+    using System;
+    using System.IO;
+    using System.Drawing;
+    using System.Windows.Forms;
+    using Microsoft.Kinect;
+
+    public partial class MainForm : Form
+    {
+
+
+        private KinectSensor sensor;
+        private System.Drawing.Graphics graphics;
+        private Bitmap body;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load_1(object sender, EventArgs e)
+        {
+            run();
+        }
+
+        private void run()
+        {
+
+            foreach (var potentialSensor in KinectSensor.KinectSensors)
+            {
+                if (potentialSensor.Status == KinectStatus.Connected)
+                {
+                    this.sensor = potentialSensor;
+                    break;
+                }
+            }
+            if (null != this.sensor)
+            {
+                this.sensor.SkeletonStream.Enable();
+                this.sensor.SkeletonFrameReady += this.SensorSkeletonFrameReady;
+
+                try
+                {
+                    this.sensor.Start();
+                } catch (IOException)
+                {
+                    this.sensor = null;
+                }
+            }
+        }
+
+        private void SensorSkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            Skeleton[] skeletons = new Skeleton[0];
+
+            using (SkeletonFrame skeletonFrame = e.OpenSkeletonFrame())
+            {
+                if (skeletonFrame != null)
+                {
+                    skeletons = new Skeleton[skeletonFrame.SkeletonArrayLength];
+                    skeletonFrame.CopySkeletonDataTo(skeletons);
+                }
+            }
+            if (skeletons.Length != 0)
+            {
+                //Initialize picture box for drawing
+                this.body = new Bitmap(pictureBox.Width, pictureBox.Height);
+                this.graphics = Graphics.FromImage(body);
+
+                System.Drawing.Rectangle fill = new System.Drawing.Rectangle(0, 0, body.Width, body.Height);
+                this.graphics.FillRectangle(Brushes.Black, fill);
+
+                foreach (Skeleton skel in skeletons)
+                {
+                    if (skel.TrackingState == SkeletonTrackingState.Tracked)
+                    {
+                        this.drawSkeletonLocationData(skel);
+                    }
+                }
+                //Push changes
+                pictureBox.Image = this.body;
+                pictureBox.Update();
+                this.body.Dispose();
+            }
+        }
+
+
+        private void drawSkeletonLocationData(Skeleton skeleton)
+        {
+            bool drawJoints = false;
+
+            //Torso
+            this.drawBone(skeleton, JointType.Head, JointType.ShoulderCenter, drawJoints);
+            this.drawBone(skeleton, JointType.ShoulderCenter, JointType.ShoulderLeft, drawJoints);
+            this.drawBone(skeleton, JointType.ShoulderCenter, JointType.ShoulderRight, drawJoints);
+            this.drawBone(skeleton, JointType.ShoulderCenter, JointType.Spine, drawJoints);
+            this.drawBone(skeleton, JointType.Spine, JointType.HipCenter, drawJoints);
+            this.drawBone(skeleton, JointType.HipCenter, JointType.HipLeft, drawJoints);
+            this.drawBone(skeleton, JointType.HipCenter, JointType.HipRight, drawJoints);
+
+            //Left arm
+            this.drawBone(skeleton, JointType.ShoulderLeft, JointType.ElbowLeft, drawJoints);
+            this.drawBone(skeleton, JointType.ElbowLeft, JointType.WristLeft, drawJoints);
+            this.drawBone(skeleton, JointType.WristLeft, JointType.HandLeft, drawJoints);
+
+            //Right arm
+            this.drawBone(skeleton, JointType.ShoulderRight, JointType.ElbowRight, drawJoints);
+            this.drawBone(skeleton, JointType.ElbowRight, JointType.WristRight, drawJoints);
+            this.drawBone(skeleton, JointType.WristRight, JointType.HandRight, drawJoints);
+
+            //Left leg
+            this.drawBone(skeleton, JointType.HipLeft, JointType.KneeLeft, drawJoints);
+            this.drawBone(skeleton, JointType.KneeLeft, JointType.AnkleLeft, drawJoints);
+            this.drawBone(skeleton, JointType.AnkleLeft, JointType.FootLeft, drawJoints);
+
+            //Right leg
+            this.drawBone(skeleton, JointType.HipRight, JointType.KneeRight, drawJoints);
+            this.drawBone(skeleton, JointType.KneeRight, JointType.AnkleRight, drawJoints);
+            this.drawBone(skeleton, JointType.AnkleRight, JointType.FootRight, drawJoints);
+        }
+
+        private Pen bonePen = new Pen(Brushes.Green, 5);
+
+        private void drawBone(Skeleton skele, JointType jointA, JointType jointB, bool drawJoints)
+        {
+            int x_offset = (pictureBox.Width / 2);
+            int y_offset = (pictureBox.Height / 2);
+            int scale = 150;
+
+            int x1_pos = (int)(skele.Joints[jointA].Position.X * scale) + x_offset;
+            int y1_pos = (int)(-skele.Joints[jointA].Position.Y * scale) + y_offset;
+            int x2_pos = (int)(skele.Joints[jointB].Position.X * scale) + x_offset;
+            int y2_pos = (int)(-skele.Joints[jointB].Position.Y * scale) + y_offset;
+
+            Point pointA = new Point(x1_pos, y1_pos);
+            Point pointB = new Point(x2_pos, y2_pos);
+
+            graphics.DrawLine(this.bonePen, pointA, pointB);
+
+            if(drawJoints)
+            {
+                System.Drawing.Rectangle rect1 = new System.Drawing.Rectangle(x1_pos - 5, y1_pos - 5, 10, 10);
+                System.Drawing.Rectangle rect2 = new System.Drawing.Rectangle(x2_pos - 5, y2_pos - 5, 10, 10);
+                graphics.FillEllipse(Brushes.Green, rect1);
+                graphics.FillEllipse(Brushes.Green, rect2);
+            }
+        }
+    }
+}
